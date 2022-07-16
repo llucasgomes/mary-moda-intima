19066cbb4d0f88f36d5e746d2ace1916ff0a8473
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-07-15 23:28:34 -0400

    CHORE: Atualização de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/template/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './componets/template/footer/footer.component';
+import { BannerComponent } from './componets/template/banner/banner.component';
+import { ProductsComponent } from './componets/template/products/products.component';
+import { CommentsComponent } from './componets/template/comments/comments.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    BannerComponent,
+    ProductsComponent,
+    CommentsComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,31 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+  export let imgProdutos = [{
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b5-640w.png" //1 imagem
+}, {
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b6-640w.png" //2 imagem
+},{
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b3-640w.png" //3 imagem
+},{
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b2-640w.png" //4 imagem
+},{
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b1-640w.png" //5 imagem
+},{
+    img: "https://lirp.cdn-website.com/004ec23b/dms3rep/multi/opt/b4-640w.png" //6 imagem
+}
+]
+
+
+export let imgComentarios=[
+    {
+        img: "https://img.cancaonova.com/cnimages/canais/uploads/sites/6/2018/03/formacao_1600x1200-como-a-presenca-da-mulher-pode-ser-harmonia-no-mundo.jpg" //1 imagem
+    },{
+        img: "https://www.pdt.org.br/wp-content/uploads/2021/01/Ana-Moro-assume-a-presid%C3%AAncia-da-A%C3%A7%C3%A3o-da-Mulher-Trabalhista-no-Paran%C3%A1.jpeg" //2 imagem
+    },{
+        img: "https://midias.correiobraziliense.com.br/_midias/jpg/2021/03/05/675x450/1_cbpfot020320212188-6556336.jpg" //6 imagem
+    }
+]
